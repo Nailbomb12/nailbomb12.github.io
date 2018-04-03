e7325cd98dc0984e8dbebb4c56bb5d60255ae779
Vitalii <nailbomb12@gmail.com>
2018-04-03 15:39:27 +0300

    fix05

--- goit-fe-course/java_script/modul-05/script.js
+++ goit-fe-course/java_script/modul-05/script.js
@@ -3,8 +3,8 @@ const alphabetRu = "йцукенгшщзхъфывапролджэячсмить
 const alphabetUa = "йцукенгшщзхїфівапролджєячсмитьбю.";
 
 function addKeyboardLayout(lang, indexFirst, indexAnother) {
-  let arr = lang.split('');
-  let lineArray = [arr.slice(0, arr.indexOf(indexFirst)), 
+  const arr = lang.split('');
+  const lineArray = [arr.slice(0, arr.indexOf(indexFirst)), 
                    arr.slice(arr.indexOf(indexFirst), arr.indexOf(indexAnother)),
                    arr.slice(arr.indexOf(indexAnother))];
   return lineArray;
@@ -38,39 +38,39 @@ const keyboard = {
 
 function setObj(obj, lang, arr){
   let keysArr = Object.keys(obj.layouts[lang]);
-  keysArr.map((val,idx) => 
-    obj.layouts[lang][val] = arr[idx])
+  for(let i = 0; i <= 2; i++){
+    obj.layouts[lang][keysArr[i]] = arr[i];
+  }
 }
 setObj(keyboard, 'en', keybEn)
 setObj(keyboard, 'ru', keybRu)
 setObj(keyboard, 'ua', keybUa)
- 
-function chooseLanguage(obj) {
-  let numb;
-  do {
-    numb = prompt('Please choose language \n0 - En, 1 - Ru, 2 - Ua', '');
 
-    if (numb === '0' || numb === '1' || numb === '2') break;
-    if (numb !== '0' || numb !== '1' || numb !== '2') {
+function chooseLanguage(obj) {
+  let langChoise = '';
+  do {
+    langChoise = prompt('Please choose language \n0 - En, 1 - Ru, 2 - Ua', '');
+    if (langChoise === '0' || langChoise === '1' || langChoise === '2') break;
+    if (langChoise === null) return; 
       alert ('Please enter correct number');
-    }
-  } while (numb !== null);
-  
-  return obj.currentLang = obj.langs[numb];
+  } while (true);
+  return obj.currentLang = obj.langs[langChoise];
 }
 
 chooseLanguage(keyboard)
 
 function getRandomInt(index) {
-  return Math.floor(Math.random() * index);
+  return Math.floor(Math.random() * index.length);
 }
 
 function getRandCharInAlph (obj){
+  if (obj.currentLang == '') return;
+  
 	let currentLangValue = Object.values(obj.layouts[obj.currentLang]);
-  let randRow = getRandomInt(currentLangValue.length);
-  let randSymb = getRandomInt(currentLangValue[randRow].length);
+  let randRow = getRandomInt(currentLangValue);
+  let randSymb = getRandomInt(currentLangValue[randRow]);
   return currentLangValue[randRow][randSymb];
 }
-let randomChar = getRandCharInAlph(keyboard);
+getRandCharInAlph(keyboard);
 
-console.log(`${randomChar} - случайный символ из алфавита ${keyboard.currentLang}`)
+console.log(`${getRandCharInAlph(keyboard)} - случайный символ из алфавита ${keyboard.currentLang}`)
